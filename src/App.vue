9bf4f449eb7badf1e220c83e95e352319121439a
Caleb Denio <cjdenio44@gmail.com>
2020-10-05 19:09:44 -0400

    ✨ Stuff

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,42 @@
   <div id="app">
     <v-app>
       <div id="content">
-        <div id="buttons">
-          <v-btn color="primary">Save</v-btn>
-          <v-btn color="error">Clear</v-btn>
+        <div class="toolbar buttons">
+          <div
+            :style="{
+              width: '300px',
+              margin: '10px auto 0 auto',
+              display: 'flex',
+              alignItems: 'flex-start',
+            }"
+          >
+            <v-slider
+              min="1"
+              max="10"
+              v-model="lineWidth"
+              thumb-label="always"
+              label="Width"
+            />
+            <canvas
+              height="10"
+              width="10"
+              :style="{ border: '1px solid white', margin: '10px 0 0 15px' }"
+              ref="lineWidthCanvas"
+            ></canvas>
+          </div>
+          <v-btn-toggle v-model="color" mandatory :style="{ marginTop: '0px' }">
+            <v-btn v-for="color in colors" :key="color">
+              <v-icon :style="{ height: lineWidth + 'px' }" :color="color"
+                >mdi-checkbox-blank-circle</v-icon
+              >
+            </v-btn>
+          </v-btn-toggle>
         </div>
         <canvas ref="canvas" id="canvas" width="512" height="512"></canvas>
+        <div class="buttons">
+          <v-btn color="primary">Save</v-btn>
+          <v-btn color="error" @click="clear()">Clear</v-btn>
+        </div>
       </div>
     </v-app>
   </div>
@@ -15,15 +46,49 @@
 <script>
 export default {
   name: "App",
-  mounted() {
+  data: () => ({
+    color: 0,
+    colors: ["#ff0000", "#00ff00", "#0000ff"],
+    lineWidth: 5,
+  }),
+  watch: {
+    lineWidth(v) {
+      let canvas = this.$refs.lineWidthCanvas;
+      let ctx = canvas.getContext("2d");
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    },
+  },
+  methods: {
+    async init() {
+      return new Promise((resolve, reject) => {
+        let canvas = this.$refs.canvas;
+        let ctx = canvas.getContext("2d");
+
+        let img = document.createElement("img");
+        img.src = "https://avatars3.githubusercontent.com/u/34525547";
+        img.addEventListener("load", () => {
+          ctx.drawImage(img, 0, 0, 512, 512);
+          resolve();
+        });
+      });
+    },
+    clear() {
+      let canvas = this.$refs.canvas;
+      let ctx = canvas.getContext("2d");
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      this.init();
+    },
+  },
+  async mounted() {
+    const self = this;
+
     let canvas = this.$refs.canvas;
     let ctx = canvas.getContext("2d");
 
-    let img = document.createElement("img");
-    img.src = "https://avatars3.githubusercontent.com/u/34525547";
-    img.addEventListener("load", () => {
-      ctx.drawImage(img, 0, 0, 512, 512);
-    });
+    await this.init();
 
     // Drawing stuff
 
@@ -47,9 +112,9 @@ export default {
 
       ctx.beginPath(); // begin
 
-      ctx.lineWidth = 5;
+      ctx.lineWidth = self.lineWidth;
       ctx.lineCap = "round";
-      ctx.strokeStyle = "#c0392b";
+      ctx.strokeStyle = self.colors[self.color];
 
       ctx.moveTo(pos.x, pos.y); // from
       setPosition(e);
@@ -84,7 +149,18 @@ body {
   text-align: center;
 }
 
-#buttons {
-  margin-bottom: 20px;
+.buttons > * {
+  margin: 10px 5px;
+  text-align: center;
 }
+
+#canvas {
+  cursor: url("./assets/icons8-pencil-24.png"), default;
+}
+
+/* .toolbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+} */
 </style>
